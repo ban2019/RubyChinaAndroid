d8f395ca2eada036f1b59be50fd45647ce948747
xialin001 <waffe3waffe3@gmail.com>
2015-10-08 14:12:10 +0800

    add card view for post fragment and adjust some padding and margin

--- app/src/main/java/org/rubychinaandroid/fragments/PostFragment.java
+++ app/src/main/java/org/rubychinaandroid/fragments/PostFragment.java
@@ -5,6 +5,7 @@ import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.CardView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
