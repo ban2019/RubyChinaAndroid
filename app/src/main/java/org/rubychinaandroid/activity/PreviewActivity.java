4a9ed842efffb8863fe66d9fb03bf1af59b4c7c6
xialin001 <waffe3waffe3@gmail.com>
2015-10-16 16:59:54 +0800

    use bypass to preview markdown

--- app/src/main/java/org/rubychinaandroid/activity/PreviewActivity.java
+++ app/src/main/java/org/rubychinaandroid/activity/PreviewActivity.java
@@ -2,9 +2,14 @@ package org.rubychinaandroid.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
+import org.rubychinaandroid.R;
 import org.rubychinaandroid.utils.RubyChinaArgKeys;
 
+import in.uncod.android.bypass.Bypass;
 import me.imid.swipebacklayout.lib.app.SwipeBackActivity;
 import us.feras.mdv.MarkdownView;
 
@@ -14,13 +19,18 @@ public class PreviewActivity extends SwipeBackActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preview);
 
-        MarkdownView markdownView = new MarkdownView(this);
-
-        setContentView(markdownView);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        toolbar.setTitle("预览");
 
         Intent intent = getIntent();
         String content = intent.getStringExtra(RubyChinaArgKeys.POST_CONTENT);
-        markdownView.loadMarkdown(content);
+
+        TextView preview = (TextView) findViewById(R.id.text_view);
+        Bypass bypass = new Bypass();
+        CharSequence string = bypass.markdownToSpannable(content);
+        preview.setText(string);
+        preview.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
